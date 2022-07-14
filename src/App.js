a1afdc4299416433352d7a2b3f814606815bc389
Hiep Xuan Nguyen <hiep.xuannguyen@successsoftware.global>
2022-07-14 14:26:22 +0700

    1st

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Canvas, useLoader, useFrame } from "@react-three/fiber";
+import { Suspense, useRef } from "react";
+import { BackSide } from "three";
+import { TextureLoader } from "three/src/loaders/TextureLoader.js";
+
+import "./App.css";
+
+function Box(props) {
+  const mesh = useRef();
+  const textures = useLoader(TextureLoader, [
+    "./images/px.jpg",
+    "./images/nx.jpg",
+    "./images/py.jpg",
+    "./images/ny.jpg",
+    "./images/pz.jpg",
+    "./images/nz.jpg",
+  ]);
+
+  // useFrame(() => {
+  //   mesh.current.rotation.x = mesh.current.rotation.y += 0.005;
+  // });
+
+  return (
+    <mesh ref={mesh} position={[0, 0, 0]} rotation={[0, 0, 0]} scale={1}>
+      <boxBufferGeometry attach="geometry" args={[1000, 1000, 1000]} />
+
+      {textures.map((item, index) => (
+        <meshPhongMaterial
+          attachArray="material"
+          side={BackSide}
+          map={item}
+          key={index}
+        />
+      ))}
+    </mesh>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas className="canvas">
+        <ambientLight intensity={0.5} />
+
+        <OrbitControls target={[0, 0, 0]} enableZoom={false} />
+        <PerspectiveCamera position={[0, 0, 0]} />
+        <Suspense fallback={null}>
+          <Box />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
